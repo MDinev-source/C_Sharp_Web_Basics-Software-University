7b1ef1ad71bb9e34bcf26e449d4aca22525c85ec
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-06-08 09:23:56 +0300

    Implement Entities and Add Migration

--- Shared Trip - 16 Feb 2020/Shared Trip - Solution/Apps/SharedTrip/Data/ApplicationDbContext.cs
+++ Shared Trip - 16 Feb 2020/Shared Trip - Solution/Apps/SharedTrip/Data/ApplicationDbContext.cs
@@ -12,5 +12,23 @@
             : base(db)
         {
         }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+          if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=DESKTOP-2R59647\SQLEXPRESS;Database=SharedTrip;Integrated Security=True");
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserTrip>().HasKey(x => new
+            {
+                x.TripId,
+                x.UserId
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
