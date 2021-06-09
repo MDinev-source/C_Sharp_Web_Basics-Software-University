f27b1b4ec251abeb13e7c124568bb1f57bd7bd1a
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-06-09 09:34:46 +0300

    Validate Login and Register

--- Shared Trip Project/Shared Trip - Solution/Apps/SharedTrip/Controllers/UsersController.cs
+++ Shared Trip Project/Shared Trip - Solution/Apps/SharedTrip/Controllers/UsersController.cs
@@ -1,10 +1,19 @@
 ﻿namespace SharedTrip.Controllers
 {
+    using SharedTrip.Services;
     using SharedTrip.ViewModels.Users;
     using SUS.HTTP;
     using SUS.MvcFramework;
+    using System.ComponentModel.DataAnnotations;
+
     public class UsersController : Controller
     {
+        private readonly IUsersService usersService;
+
+        public UsersController(IUsersService usersService)
+        {
+            this.usersService = usersService;
+        }
         public HttpResponse Login()
         {
             return this.View();
@@ -13,7 +22,16 @@
         [HttpPost]
         public HttpResponse Login(LoginInputModel input)
         {
+            var userId = this.usersService.GetUserId(input.Username, input.Password);
 
+            if (userId == null)
+            {
+                return this.Error("Invalid username or password");
+            }
+
+            this.SignIn(userId);
+
+            return this.Redirect("/Traips/All");
         }
         public HttpResponse Register()
         {
@@ -23,7 +41,34 @@
         [HttpPost]
         public HttpResponse Register (RegisterInputModel input)
         {
+            if (string.IsNullOrEmpty(input.Username)
+                ||input.Username.Length<5
+                || input.Username.Length > 20)
+            {
+                return this.Error("Username should be between 5 and 20 characters long.");
+            }
 
+            if (string.IsNullOrEmpty(input.Email)
+                || !new EmailAddressAttribute().IsValid(input.Email))
+            {
+                return this.Error("Invalid email.");
+            }
+
+            if (string.IsNullOrEmpty(input.Password)
+               || input.Username.Length < 6
+               || input.Username.Length > 20)
+            {
+                return this.Error("Password is required and should be between 6 and 20 characters long.");
+            }
+
+            if (input.ConfirmPassword != input.Password)
+            {
+                return this.Error("Passwords do not match");
+            }
+
+            this.usersService.Create(input.Username, input.Email, input.Password);
+
+            return this.Redirect("/Users/Login");
         }
     }
 }
