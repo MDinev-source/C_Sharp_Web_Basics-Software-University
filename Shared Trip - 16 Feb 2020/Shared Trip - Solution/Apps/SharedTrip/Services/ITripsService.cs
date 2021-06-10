ed0a7147ff2359c81f3d8762fea7ab6c7d94c7ce
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-06-10 09:05:22 +0300

    Implement Details

---
+++ Shared Trip - 16 Feb 2020/Shared Trip - Solution/Apps/SharedTrip/Services/ITripsService.cs
@@ -0,0 +1,12 @@
+﻿namespace SharedTrip.Services
+{
+    using SharedTrip.ViewModels.Trips;
+    using System.Collections.Generic;
+
+    public interface ITripsService
+    {
+        void Create(AddTripInputModel trip);
+        IEnumerable<TripViewModel> GetAll();
+        TripDetailsViewModel GetDetails(string id);
+    }
+}
