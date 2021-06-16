f602bfffdd46c3b86dbf25789f48dab9241e5868
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-06-16 09:36:03 +0300

    Directories Cleaning

--- Musaca - 18 Dec 2018/Musaca - Solution/App/Musaca.Data/DataSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Musaca.Data
-{
-    public class DataSettings
-    {
-        public const string ConnectionString
-            = @"Server=.\SQLEXPRESS;Database=MusacaDB;Integrated Security=True;";
-    }
-}
