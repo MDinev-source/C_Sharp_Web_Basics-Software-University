5d35713d783ce8b3c73910272a3c927340816ac3
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-06-17 08:03:10 +0300

    Implement User Service

---
+++ Battle Cards - 28 Apr 2020/Battle Cards - solution/BattleCards/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿namespace BattleCards.Services
+{
+    public interface IUserService
+    {
+        string GetUserId(string username, string password);
+        void Register(string username, string email, string password);
+        bool IsUsernameExists(string username);
+        bool IsEmailExists(string email);
+    }
+}
