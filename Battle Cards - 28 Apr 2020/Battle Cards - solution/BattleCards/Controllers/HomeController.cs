295995a849813d76a42eabf38032ffe82ccb1967
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-06-17 07:49:27 +0300

    Implement Users Input Models

--- Battle Cards - 28 Apr 2020/Battle Cards - solution/BattleCards/Controllers/HomeController.cs
+++ Battle Cards - 28 Apr 2020/Battle Cards - solution/BattleCards/Controllers/HomeController.cs
@@ -4,9 +4,14 @@
     using SIS.MvcFramework;
 
     public class HomeController : Controller
-    { 
+    {
+        [HttpGet("/")]
         public HttpResponse Index()
         {
+            if (this.IsUserLoggedIn())
+            {
+                return this.Redirect("/Cards/All");
+            }
             return this.View();
         }
     }
