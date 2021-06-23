64f4414abdf02d013235d7f0af523066f19486d2
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-06-23 12:09:35 +0300

    Create Database

---
+++ Car Shop - 23 Dec 2020/Car Shop - Solution/Apps/CarShop/Data/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace CarShop.Data.Models
+{
+    using SUS.MvcFramework;
+    using System;
+    public class User : IdentityUser<string>
+    {
+        public User()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+        public bool IsMechanic { get; set; }
+    }
+}
