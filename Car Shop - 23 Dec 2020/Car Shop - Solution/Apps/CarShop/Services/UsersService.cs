068e59962003e81b4326596835cb00dcca908fcc
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-06-23 12:31:08 +0300

    Imlement User Service

--- Car Shop - 23 Dec 2020/Car Shop - Solution/Apps/CarShop/Services/UsersService.cs
+++ Car Shop - 23 Dec 2020/Car Shop - Solution/Apps/CarShop/Services/UsersService.cs
@@ -1,32 +1,62 @@
-﻿using CarShop.Data;
-
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-
-namespace CarShop.Services
+﻿namespace CarShop.Services
 {
-    public class UsersService: IUsersService
-    {
+    using CarShop.Data;
+    using CarShop.Data.Models;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
 
+    public class UsersService : IUsersService
+    {
+        private readonly ApplicationDbContext db;
+
+        public UsersService(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
         public void Create(string username, string email, string password, string userType)
         {
-            throw new System.NotImplementedException();
+            var user = new User
+            {
+                Username = username,
+                Email = email,
+                Password = ComputeHash (password)
+            };
+
+            this.db.Users.Add(user);
+            this.db.SaveChanges();
         }
 
         public string GetUserId(string username, string password)
         {
-            throw new System.NotImplementedException();
+            var hashPassword = ComputeHash(password);
+
+            var user = this.db.Users.FirstOrDefault(x => x.Username == username && x.Password == hashPassword);
+
+            return user?.Id;
         }
 
         public bool IsUserMechanic(string Userid)
         {
-            throw new System.NotImplementedException();
+            return this.db.Users.Any(x => x.Id == Userid && x.IsMechanic);
         }
 
         public bool IsUsernameAvailable(string username)
         {
-            throw new System.NotImplementedException();
+            return this.db.Users.Any(x => x.Username == username);
+        }
+
+        private static string ComputeHash(string input)
+        {
+            var bytes = Encoding.UTF8.GetBytes(input);
+            using var hash = SHA512.Create();
+            var hashedInputBytes = hash.ComputeHash(bytes);
+            // Convert to text
+            // StringBuilder Capacity is 128, because 512 bits / 8 bits in byte * 2 symbols for byte 
+            var hashedInputStringBuilder = new StringBuilder(128);
+            foreach (var b in hashedInputBytes)
+                hashedInputStringBuilder.Append(b.ToString("X2"));
+            return hashedInputStringBuilder.ToString();
         }
     }
 }
