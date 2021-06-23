6f41770c0f0f19e834ddf99c0f887450f7a80d92
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-06-23 12:14:35 +0300

    Set User Input Models

--- Car Shop - 23 Dec 2020/Car Shop - Solution/Apps/CarShop/ViewModels/RegisterInputModel.cs
+++ Car Shop - 23 Dec 2020/Car Shop - Solution/Apps/CarShop/ViewModels/RegisterInputModel.cs
@@ -1,8 +1,10 @@
 ﻿namespace CarShop.ViewModels
 {
-    public class LoginInputModel
+    public class RegisterInputModel
     {
         public string Username { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
