d639e84bd22d677e1af5430a9dc2d27a7e2291cc
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-06-23 09:08:35 +0300

    Delete Unnecessary Directory

--- Shared Trip v2 - 16 Feb 2020/Shared Trip - Solution/Apps/SharedTrip/Data/DatabaseConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SharedTrip
-{
-    public class DatabaseConfiguration
-    {
-        public const string ConnectionString =
-            @"Integrated Security=True;Server=DESKTOP-2R59647\SQLEXPRESS;Database=SharedTrip";
-    }
-}
