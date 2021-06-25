865886a4a3ab17dfe2ff912fd954b679599db19d
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-06-25 11:17:33 +0300

    Set Database

---
+++ Git - 25 Oct 2020/Git - Solution/Apps/Git/Data/Models/Commit.cs
@@ -0,0 +1,29 @@
+﻿namespace Git.Data.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Commit
+    {
+        public Commit()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        public string CreateorId { get; set; }
+
+        public User Creator { get; set; }
+
+        [Required]
+        public string RepositoryId { get; set; }
+
+        public Repository Repository { get; set; }
+    }
+}
---
+++ Git - 25 Oct 2020/Git - Solution/Apps/Git/Data/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace Git.Data.Models
+{
+    using SUS.MvcFramework;
+    using System;
+    using System.Collections.Generic;
+    public class User : IdentityUser<string>
+    {
+        public User()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Repositories = new HashSet<Repository>();
+            this.Commits = new HashSet<Commit>();
+        }
+        public ICollection<Repository> Repositories { get; set; }
+        public ICollection<Commit> Commits { get; set; }
+    }
+}
