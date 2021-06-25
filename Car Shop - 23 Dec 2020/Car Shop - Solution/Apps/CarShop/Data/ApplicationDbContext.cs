5ea97ac3d7ca8630faa01313c2b780720e55bba8
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-06-25 18:42:57 +0300

    Create Database

---
+++ Car Shop - 23 Dec 2020/Car Shop - Solution/Apps/CarShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿namespace CarShop.Data
+{
+    using CarShop.Data.Models;
+    using Microsoft.EntityFrameworkCore;
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Issue> Issues { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=DESKTOP-2R59647\SQLEXPRESS;Database=CarShop;Integrated Security=true;");
+            }
+        }
+    }
+}
---
+++ Car Shop - 23 Dec 2020/Car Shop - Solution/Apps/CarShop/Data/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace CarShop.Data.Models
+{
+    using SUS.MvcFramework;
+    using System;
+    public class User : IdentityUser<string>
+    {
+
+        public User()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+        public bool IsMechanic { get; set; }
+    }
+}
